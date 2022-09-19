493ccdb56d2dbf134fcf7f44c11c39b117e0fabf
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2022-09-19 16:10:46 +0200

    feat: Add decorative text displayed next to the game board

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,5 +1,6 @@
 export const FONTSIZE_MEDIUM = `1.4vw`;
 export const FONTSIZE_LARGE = `1.8vw`;
+export const FONTSIZE_EXTRA_LARGE = `5vw`;
 export const NEON_EFFECT_GRADIENT = `linear-gradient(
     45deg,
     #c0392b,
