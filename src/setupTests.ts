9fa396bae0cab469df919b74e01751ae6424cae8
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-02-14 23:30:38 +0100

    test: add msw test setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "./__test__/msw/server";
+
+// Enable request interception.
+beforeAll(() => server.listen());
+
+// Reset handlers so that each test could alter them
+// without affecting other, unrelated tests.
+afterEach(() => server.resetHandlers());
+
+// Don't forget to clean up afterwards.
+afterAll(() => server.close());
