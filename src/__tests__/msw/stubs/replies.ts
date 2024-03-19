95bb4b5408ad53efb53eabbbf6cfdf5a7c17b3e8
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-19 22:30:59 +0100

    test: add replies stubs to which the msw server will return in response to appropriate requests

---
+++ src/__tests__/msw/stubs/replies.ts
@@ -0,0 +1,101 @@
+import { ReplyRepresentation } from "../../../shared/models/externalApiRepresentation/Resources";
+
+import { commentsCollectionStub } from "./comments";
+import { userStub, adminStub, bannedStub } from "./users";
+
+const userRepliesCollectionStub: ReplyRepresentation[] = [
+  {
+    parentComment: commentsCollectionStub[0],
+    author: userStub,
+    body: "user reply to first comment",
+    id: 1,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[2],
+    author: userStub,
+    body: "user reply to third comment",
+    id: 2,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[4],
+    author: userStub,
+    body: "user reply to fifth comment",
+    id: 3,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+];
+
+const adminRepliesCollectionStub: ReplyRepresentation[] = [
+  {
+    parentComment: commentsCollectionStub[0],
+    author: adminStub,
+    body: "admin reply to first comment",
+    id: 4,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[2],
+    author: adminStub,
+    body: "admin reply to third comment",
+    id: 5,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[4],
+    author: adminStub,
+    body: "admin reply to fifth comment",
+    id: 6,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+];
+
+const bannedRepliesCollectionStub: ReplyRepresentation[] = [
+  {
+    parentComment: commentsCollectionStub[0],
+    author: bannedStub,
+    body: "banned reply to first comment",
+    id: 7,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[2],
+    author: bannedStub,
+    body: "banned reply to third comment",
+    id: 8,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+  {
+    parentComment: commentsCollectionStub[4],
+    author: bannedStub,
+    body: "banned reply to fifth comment",
+    id: 9,
+    createdAt: "2023-10-15T20:30:38",
+    updatedAt: "2023-10-15T20:30:38",
+  },
+];
+
+export let repliesCollectionStub: ReplyRepresentation[] = [
+  ...userRepliesCollectionStub,
+  ...adminRepliesCollectionStub,
+  ...bannedRepliesCollectionStub,
+];
+
+let repliesCollectionStubCopy: ReplyRepresentation[] = JSON.parse(
+  JSON.stringify(repliesCollectionStub)
+);
+
+export const resetRepliesCollectionStub = () => {
+  repliesCollectionStub.length = 0; // This will clear the existing array by setting its length to 0
+  repliesCollectionStub.push(...repliesCollectionStubCopy);
+  repliesCollectionStubCopy = JSON.parse(JSON.stringify(repliesCollectionStub));
+};
