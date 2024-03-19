d30f17d818d838a33ce6f5af930ad287f4e2cea7
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-19 22:47:19 +0100

    test: adapt the server to changes made in several immediately preceding commits on resource handlers

--- src/__tests__/msw/server.ts
+++ src/__tests__/msw/server.ts
@@ -1,16 +1,41 @@
 import { setupServer } from "msw/node";
-import { handlers as commentsHandlers } from "./handlers/comments";
-import { handlers as repliesHandlers } from "./handlers/replies";
-import { handlers as usersHandlers } from "./handlers/users";
-import { handlers as authHandlers } from "./handlers/auth";
-import { handlers as accountHandlers } from "./handlers/account";
+import {
+  handlers as commentsHandlers,
+  internalServerErrorHandlers as commentsInternalServerErrorHandlers,
+} from "./handlers/comments";
+import {
+  handlers as repliesHandlers,
+  internalServerErrorHandlers as repliesInternalServerErrorHandlers,
+} from "./handlers/replies";
+import {
+  handlers as usersHandlers,
+  internalServerErrorHandlers as usersInternalServerErrorHandlers,
+} from "./handlers/users";
+import {
+  handlers as authHandlers,
+  internalServerErrorHandlers as authInternalServerErrorHandlers,
+} from "./handlers/auth";
+import {
+  handlers as accountHandlers,
+  internalServerErrorHandlers as accountInternalServerErrorHandlers,
+} from "./handlers/account";
 
-// In line with the success behavior-first strategy, the basic handles describing the correct network behavior (happy paths o the network) are defined below
-// Utilize network behavior overrides to split the behavior of the same resource between its happy state and its on-demand states whenever you need them.
-export const server = setupServer(
+export const handlers = [
   ...commentsHandlers,
   ...repliesHandlers,
   ...usersHandlers,
   ...authHandlers,
-  ...accountHandlers
-);
+  ...accountHandlers,
+];
+
+export const internalServerErrorHandlers = [
+  ...commentsInternalServerErrorHandlers,
+  ...repliesInternalServerErrorHandlers,
+  ...usersInternalServerErrorHandlers,
+  ...authInternalServerErrorHandlers,
+  ...accountInternalServerErrorHandlers,
+];
+
+// In line with the success behavior-first strategy, the basic handles describing the correct network behavior (happy paths o the network) are defined below
+// Utilize network behavior overrides to split the behavior of the same resource between its happy state and its on-demand states whenever you need them.
+export const server = setupServer(...handlers);
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,15 +3,22 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-
-import { server } from "./__test__/msw/server";
+import { server } from "./__tests__/msw/server";
+import { resetCommentsCollectionStub } from "./__tests__/msw/stubs/comments";
+import { resetRepliesCollectionStub } from "./__tests__/msw/stubs/replies";
+import { resetUsersCollectionStub } from "./__tests__/msw/stubs/users";
 
 // Enable request interception.
 beforeAll(() => server.listen());
 
 // Reset handlers so that each test could alter them
 // without affecting other, unrelated tests.
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  resetUsersCollectionStub();
+  resetCommentsCollectionStub();
+  resetRepliesCollectionStub();
+  server.resetHandlers();
+});
 
 // Don't forget to clean up afterwards.
 afterAll(() => server.close());
