430c4f16b64815ba979be37ee620b90e299ec48b
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-19 22:44:02 +0100

    test: isolate individual tasks on user account resources to individual functions

--- src/__tests__/msw/handlers/account.ts
+++ src/__tests__/msw/handlers/account.ts
@@ -8,19 +8,25 @@ import { isAuthenticated } from "../helpers/isAuthenticated";
 
 import { TPUserRepresentation } from "../../../shared/models/externalApiRepresentation/Resources";
 import { DeleteAccountResponse } from "../../../shared/models/externalApiRepresentation/Responses";
-import { EditAccountRequest } from "../../../shared/models/externalApiRepresentation/Requests";
+import { PatchAccountRequest } from "../../../shared/models/externalApiRepresentation/Requests";
 import {
   BadRequest400ResponseType,
   InternalServerError500ResponseType,
   NotFound404ResponseType,
 } from "../../../shared/models/externalApiRepresentation/Errors";
 
-const getActivitySummaryPath = `${process.env.REACT_APP_TRICKYPLAY_API_BASE_URL}/${process.env.REACT_APP_ACCOUNT_URL}/${process.env.REACT_APP_ACCOUNT_ACTIVITY_SUMMARY_ENDPOINT}`;
-const accountPath = `${process.env.REACT_APP_TRICKYPLAY_API_BASE_URL}/${process.env.REACT_APP_ACCOUNT_URL}`;
+import { usersCollectionStub } from "../stubs/users";
 
-export const handlers = [
-  // get activity summary
-  http.get(getActivitySummaryPath, async ({ request, params, cookies }) => {
+import {
+  deleteAccountPath,
+  getAccountPath,
+  getActivitySummaryPath,
+  patchAccountPath,
+} from "../urls";
+
+export const getActivitySummary = http.get(
+  getActivitySummaryPath,
+  async ({ request, params, cookies }) => {
     await isAuthenticated(request, getActivitySummaryPath);
     const buffer = fs.readFileSync(
       path.resolve(process.cwd(), "__tests__/msw/stubs/activitySummaryStub.pdf")
@@ -32,88 +38,108 @@ export const handlers = [
         "Content-Disposition": "attachment; filename=activitySummaryStub.pdf",
       },
     });
-  }),
+  }
+);
 
-  // get account
-  http.get<{}, {}, TPUserRepresentation>(
-    accountPath,
-    async ({ request, params, cookies }) => {
-      await isAuthenticated(request, accountPath);
-      const response: TPUserRepresentation = {
-        id: 1,
-        name: "user",
-        createdAt: "2023-10-15T20:30:38",
-        updatedAt: "2023-10-15T20:30:38",
-        role: "USER",
-      };
-      return HttpResponse.json(response);
-    }
-  ),
-
-  // delete account
-  http.delete<{}, {}, DeleteAccountResponse>(
-    accountPath,
-    async ({ request, params, cookies }) => {
-      await isAuthenticated(request, accountPath);
-      const response: DeleteAccountResponse = {
-        message: "The account for user with id: 1 has been removed",
-      };
-      return HttpResponse.json(response);
-    }
-  ),
-
-  // edit account
-  http.patch<
-    {},
-    EditAccountRequest,
-    TPUserRepresentation | BadRequest400ResponseType
-  >(accountPath, async ({ request, params, cookies }) => {
-    await isAuthenticated(request, accountPath);
-
-    const data = await request.formData();
-    let newUsername = data.get("newUsername");
-    let newPassword = data.get("newPassword");
-
-    newUsername = newUsername as string; // FormData.get() returns a value of type string | File | null.
-    newPassword = newPassword as string;
-    if (newUsername && !new RegExp("^[a-zA-Z0-9_]{2,16}$").test(newUsername)) {
-      return HttpResponse.json(
-        generateErrorResponseBody(
-          "Bad Request",
-          accountPath,
-          "Username must contain between 2 and 16 characters. It can only consist of underscores, numbers, lowercase and uppercase letters."
-        )
+export const getAccount = http.get<{}, {}, TPUserRepresentation>(
+  getAccountPath,
+  async ({ request, params, cookies }) => {
+    const username = isAuthenticated(request, getAccountPath);
+    const signedUser = usersCollectionStub.find((u) => u.name === username);
+    if (signedUser == null) {
+      const responseBody = generateErrorResponseBody(
+        "Not Found",
+        getAccountPath,
+        "user not found"
       );
-    }
-    if (
-      newPassword &&
-      !new RegExp("^(?=.*[0-9])[a-zA-Z0-9_]{4,32}$").test(newPassword)
-    ) {
-      return HttpResponse.json(
-        generateErrorResponseBody(
-          "Bad Request",
-          accountPath,
-          "Password must contain between 4 and 32 characters. It must contain at least one number. The password can only consist of underscores, numbers, lowercase and uppercase letters."
-        )
-      );
+      throw HttpResponse.json(responseBody, { status: 404 });
     }
 
-    const response: TPUserRepresentation = {
-      id: 1,
-      name: "user",
-      createdAt: "2023-10-15T20:30:38",
-      updatedAt: "2023-10-15T20:30:38",
-      role: "USER",
+    return HttpResponse.json(signedUser);
+  }
+);
+
+export const deleteAccount = http.delete<{}, {}, DeleteAccountResponse>(
+  deleteAccountPath,
+  async ({ request, params, cookies }) => {
+    const username = isAuthenticated(request, deleteAccountPath);
+    const signedUser = usersCollectionStub.find((u) => u.name === username);
+    if (signedUser == null) {
+      const responseBody = generateErrorResponseBody(
+        "Not Found",
+        deleteAccountPath,
+        "user not found"
+      );
+      throw HttpResponse.json(responseBody, { status: 404 });
+    }
+
+    usersCollectionStub.splice(
+      usersCollectionStub.findIndex((u) => u.id === signedUser.id),
+      1
+    );
+
+    const response: DeleteAccountResponse = {
+      message: `The account for user with id: ${signedUser.id} has been removed`,
     };
-
-    if (newUsername) {
-      response.name = newUsername as string;
-      response.updatedAt = "2023-10-20T22:32:30";
-    }
-
     return HttpResponse.json(response);
-  }),
-];
+  }
+);
+
+export const patchAccount = http.patch<
+  {},
+  PatchAccountRequest,
+  TPUserRepresentation | BadRequest400ResponseType
+>(patchAccountPath, async ({ request, params, cookies }) => {
+  const username = isAuthenticated(request, patchAccountPath);
+
+  const data = await request.formData();
+  let newUsername = data.get("newUsername");
+  let newPassword = data.get("newPassword");
+
+  newUsername = newUsername as string; // FormData.get() returns a value of type string | File | null.
+  newPassword = newPassword as string;
+  if (newUsername && !/^[a-zA-Z0-9_]{2,16}$/.test(newUsername)) {
+    return HttpResponse.json(
+      generateErrorResponseBody(
+        "Bad Request",
+        patchAccountPath,
+        "Username must contain between 2 and 16 characters. It can only consist of underscores, numbers, lowercase and uppercase letters."
+      )
+    );
+  }
+  if (newPassword && !/^(?=.*[0-9])[a-zA-Z0-9_]{4,32}$/.test(newPassword)) {
+    return HttpResponse.json(
+      generateErrorResponseBody(
+        "Bad Request",
+        patchAccountPath,
+        "Password must contain between 4 and 32 characters. It must contain at least one number. The password can only consist of underscores, numbers, lowercase and uppercase letters."
+      )
+    );
+  }
+
+  const signedUser = usersCollectionStub.find((user) => user.name === username);
+  if (signedUser == null) {
+    const responseBody = generateErrorResponseBody(
+      "Not Found",
+      patchAccountPath,
+      "user not found"
+    );
+    throw HttpResponse.json(responseBody, { status: 404 });
+  }
+
+  const updatedAt = new Date();
+
+  if (newUsername) {
+    signedUser.name = newUsername as string;
+    signedUser.updatedAt = updatedAt.toISOString().split(".")[0];
+  }
+
+  if (newPassword) {
+    signedUser.updatedAt = updatedAt.toISOString().split(".")[0];
+  }
+
+  return HttpResponse.json(signedUser);
+});
 
 export const getActivitySummary_UserNotFound = http.get<
   {},
@@ -121,7 +147,11 @@ export const getActivitySummary_UserNotFound = http.get<
   NotFound404ResponseType
 >(getActivitySummaryPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
-    generateErrorResponseBody("Not Found", accountPath, "user not found"),
+    generateErrorResponseBody(
+      "Not Found",
+      getActivitySummaryPath,
+      "user not found"
+    ),
     { status: 404 }
   );
 });
@@ -130,20 +160,20 @@ export const getAccount_UserNotFound = http.get<
   {},
   {},
   NotFound404ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(getAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
-    generateErrorResponseBody("Not Found", accountPath, "user not found"),
+    generateErrorResponseBody("Not Found", getAccountPath, "user not found"),
     { status: 404 }
   );
 });
 
-export const editAccount_UserNotFound = http.patch<
+export const patchAccount_UserNotFound = http.patch<
   {},
-  EditAccountRequest,
+  PatchAccountRequest,
   NotFound404ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(patchAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
-    generateErrorResponseBody("Not Found", accountPath, "user not found"),
+    generateErrorResponseBody("Not Found", patchAccountPath, "user not found"),
     { status: 404 }
   );
 });
@@ -152,9 +182,9 @@ export const deleteAccount_UserNotFound = http.delete<
   {},
   {},
   NotFound404ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(deleteAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
-    generateErrorResponseBody("Not Found", accountPath, "user not found"),
+    generateErrorResponseBody("Not Found", deleteAccountPath, "user not found"),
     { status: 404 }
   );
 });
@@ -178,26 +208,26 @@ export const getAccount_InternalServerError = http.get<
   {},
   {},
   InternalServerError500ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(getAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
     generateErrorResponseBody(
       "Internal Server Error",
-      accountPath,
+      getAccountPath,
       "Internal Server Error"
     ),
     { status: 500 }
   );
 });
 
-export const editAccount_InternalServerError = http.patch<
+export const patchAccount_InternalServerError = http.patch<
   {},
-  EditAccountRequest,
+  PatchAccountRequest,
   InternalServerError500ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(patchAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
     generateErrorResponseBody(
       "Internal Server Error",
-      accountPath,
+      patchAccountPath,
       "Internal Server Error"
     ),
     { status: 500 }
@@ -208,13 +238,34 @@ export const deleteAccount_InternalServerError = http.delete<
   {},
   {},
   InternalServerError500ResponseType
->(accountPath, async ({ params, request, cookies }) => {
+>(deleteAccountPath, async ({ params, request, cookies }) => {
   return HttpResponse.json(
     generateErrorResponseBody(
       "Internal Server Error",
-      accountPath,
+      deleteAccountPath,
       "Internal Server Error"
     ),
     { status: 500 }
   );
 });
+
+export const handlers = [
+  getActivitySummary,
+  getAccount,
+  deleteAccount,
+  patchAccount,
+];
+
+export const internalServerErrorHandlers = [
+  getActivitySummary_InternalServerError,
+  getAccount_InternalServerError,
+  deleteAccount_InternalServerError,
+  patchAccount_InternalServerError,
+];
+
+export const userNotFoundHandlers = [
+  getActivitySummary_UserNotFound,
+  getAccount_UserNotFound,
+  deleteAccount_UserNotFound,
+  patchAccount_UserNotFound,
+];
