25a3e07295cd41a219d0c8322abfbdcb144d686a
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-30 00:04:15 +0100

    feat: add responsive resizing to the action button, adjust its colors to the website's color palette

--- src/shared/components/ActionButton/index.tsx
+++ src/shared/components/ActionButton/index.tsx
@@ -1,65 +1,105 @@
-import styled from "styled-components";
-import * as palette from "../../../styles/variables";
+import { Box } from "@mui/material";
 
-//neon button
-export default styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+type Props = { onClick: () => void; children: React.ReactNode };
 
-  cursor: pointer;
+const ActionButton = ({ onClick, children }: Props) => {
+  return (
+    <Box
+      component="button"
+      onClick={onClick}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
 
-  background-color: rgba(0, 0, 0, 0);
-  border: none;
-  position: relative;
-  padding: 10px 30px;
-  margin: 0 15px;
-  color: ${(p) => p.theme.primaryColor};
-  text-decoration: none;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-size: ${palette.FONTSIZE_MEDIUM};
-  transition: 0.5s;
-  overflow: hidden;
-  -webkit-box-reflect: below 1px linear-gradient(transparent, #0003);
-  &:hover {
-    background: ${(p) => p.theme.primaryColor};
-    color: ${(p) => p.theme.backgroundColor};
-    box-shadow: ${(p) => `0 0 50px ${p.theme.primaryColor}`};
-    transition-delay: 0.5s;
-  }
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 10px;
-    height: 10px;
-    border-top: ${(p) => `2px solid ${p.theme.primaryColor}`};
-    border-left: ${(p) => `2px solid ${p.theme.primaryColor}`};
-    transition: 0.5s;
-    transition-delay: 0.5s;
-  }
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    width: 10px;
-    height: 10px;
-    border-bottom: ${(p) => `2px solid ${p.theme.primaryColor}`};
-    border-right: ${(p) => `2px solid ${p.theme.primaryColor}`};
-    transition: 0.5s;
-    transition-delay: 0.5s;
-  }
-  &:hover::before {
-    width: 100%;
-    height: 100%;
-    transition-delay: 0s;
-  }
-  &:hover::after {
-    width: 100%;
-    height: 100%;
-    transition-delay: 0s;
-  }
-`;
+        cursor: "pointer",
+
+        backgroundColor: "rgba(0, 0, 0, 0)",
+
+        border: "none",
+
+        position: "relative",
+
+        padding: "10px 30px",
+        margin: "10px 15px",
+
+        color: "primary.main",
+        textDecoration: "none",
+        textTransform: "uppercase",
+        letterSpacing: "1px",
+        fontSize: { xs: "10px", sm: "20px", md: "25px", lg: "30px" },
+
+        transition: "0.4s",
+
+        overflow: "hidden",
+
+        "&:hover": {
+          backgroundColor: "primary.main",
+          color: "#000000",
+          boxShadow: "0 0 50px primary.main",
+          transitionDelay: "0.5s",
+        },
+
+        "&::before": {
+          content: '""',
+
+          position: "absolute",
+          top: 0,
+          left: 0,
+
+          width: "10px",
+          height: "10px",
+
+          // borderTop: "2px solid primary.main",
+          // borderLeft: "2px solid primary.main",
+          borderLeftStyle: "solid",
+          borderLeftWidth: "2",
+          borderLeftColor: "primary.main",
+          borderTopStyle: "solid",
+          borderTopWidth: "2",
+          borderTopColor: "primary.main",
+
+          transition: "0.5s",
+          transitionDelay: "0.5s",
+        },
+        "&::after": {
+          content: '""',
+
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+
+          width: "10px",
+          height: "10px",
+
+          // borderBottom: `2px solid primary.main`,
+          // borderRight: `2px solid primary.main`,
+          borderBottomStyle: "solid",
+          borderBottomWidth: "2",
+          borderBottomColor: "primary.main",
+          borderRightStyle: "solid",
+          borderRightWidth: "2",
+          borderRightColor: "primary.main",
+
+          transition: "0.5s",
+          transitionDelay: "0.5s",
+        },
+
+        "&:hover::before": {
+          width: "100%",
+          height: "100%",
+          transitionDelay: "0s",
+        },
+        "&:hover::after": {
+          width: "100%",
+          height: "100%",
+          transitionDelay: "0s",
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default ActionButton;
