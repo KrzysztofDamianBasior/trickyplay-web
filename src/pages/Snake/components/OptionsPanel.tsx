5e8882df014204866a7ececf49e63fc4661cf911
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-31 00:27:19 +0100

    feat: add responsive behaviors to the snake game options panel

--- src/pages/Snake/components/OptionsPanel.tsx
+++ src/pages/Snake/components/OptionsPanel.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-import StatefulButton from "../../../shared/components/StatefulButton";
 import { useDarkMode } from "usehooks-ts";
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+import StatefulButton from "../../../shared/components/StatefulButton";
 
 type Props = {
   optionsState: {
@@ -22,8 +24,17 @@ const OptionsPanel = ({ optionsState, setOptionsState }: Props) => {
   return (
     <OptionsPanelContainer>
       <OptionContainer>
-        <label>difficulty level:</label>
-        <ToggleSwitchContainer>
+        <Box
+          component="label"
+          htmlFor="difficultyLevel"
+          sx={{
+            marginRight: "15px",
+            fontSize: { xs: "12px", sm: "20px", md: "20px", lg: "25px" },
+          }}
+        >
+          Difficulty level:
+        </Box>
+        <ToggleSwitchContainer id="difficultyLevel">
           {["easy", "medium", "hard"].map((label) => (
             <StatefulButton
               isActive={optionsState.difficultyLevel === label ? true : false}
@@ -54,31 +65,30 @@ const OptionsPanel = ({ optionsState, setOptionsState }: Props) => {
 };
 export default OptionsPanel;
 
-const OptionsPanelContainer = styled.div`
-  width: 30vw;
+const OptionsPanelContainer = styled("div")`
   height: 100%;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
-const OptionContainer = styled.div`
+
+const OptionContainer = styled("div")`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
   width: 100%;
   margin-top: 2%;
-  label {
-    margin-right: 20px;
-  }
 `;
 
-const ToggleSwitchContainer = styled.div`
+const ToggleSwitchContainer = styled("div")`
   align-self: flex-end;
-  position: relative;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
+
+  position: relative;
 `;
