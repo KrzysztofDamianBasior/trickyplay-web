e0b85217c9222712e8c3b1eaa18e8d50b6e5b031
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-31 00:26:22 +0100

    feat: add responsive behaviors to the snake game dashboard, adjust the color palette to the color theme of the website

--- src/pages/Snake/components/ControlsPanel.tsx
+++ src/pages/Snake/components/ControlsPanel.tsx
@@ -1,74 +1,99 @@
-import React from "react";
-import styled from "styled-components";
+import { useDarkMode } from "usehooks-ts";
+import { styled } from "@mui/material/styles";
+
 import ActionButton from "../../../shared/components/ActionButton";
 import StatefulButton from "../../../shared/components/StatefulButton";
 import Banner from "../../../shared/components/Banner";
-import { useDarkMode } from "usehooks-ts";
+import { Box } from "@mui/material";
 
 type Props = {
   score: number;
-  openModal: () => void;
+  disabled: boolean;
   pause: boolean;
   setPause: () => void;
-  disabled: boolean;
+  openModal: () => void;
 };
 
 const ControlsPanel = ({
   score,
-  openModal,
+  disabled,
   pause,
   setPause,
-  disabled,
+  openModal,
 }: Props) => {
   const { isDarkMode } = useDarkMode();
 
   return (
     <SnakeControls>
       <Banner text="Snake" />
-      <article>
-        <section>
-          <ActionButton onClick={() => openModal()}>
-            Start a new game
-          </ActionButton>
-        </section>
-        <section>points: {score}</section>
-        <section>
-          <StatefulButton
-            isDarkMode={isDarkMode}
-            isActive={pause && !disabled}
-            onClick={() => {
-              if (!disabled) setPause();
+      <Box
+        sx={{
+          backgroundImage: (theme) =>
+            theme.palette.mode === "light"
+              ? `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" fill="%23000000"><path d="M1912 1209c27-319-72-699-318-882-187-139-462-224-785-204S85 468 84 880c0 455 481 345 636 484s139 514 477 516c386 3 693-420 715-671Z"></path></svg>')`
+              : "",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "70%",
+          backgroundPosition: "center",
+          padding: { xs: "80px", sm: "120x", md: "140px" },
+          // margin: "50px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <ActionButton onClick={() => openModal()}>
+          Start a new game
+        </ActionButton>
+        <Box
+          sx={{
+            backgroundColor: "secondary.dark",
+            borderRadius: "16px",
+            p: 1,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "column",
+            width: "80%",
+            minHeight: "150px",
+          }}
+        >
+          <Box
+            sx={{
+              color: "secondary.contrastText",
+              width: "100%",
+              fontSize: { xs: "12px", sm: "20px", md: "20px", lg: "25px" },
+              textTransform: "uppercase",
+              textAlign: "center",
+              m: 1,
             }}
           >
-            Pause
-          </StatefulButton>
-        </section>
-      </article>
+            points: {score}
+          </Box>
+          <Box sx={{ backgroundColor: "secondary.light" }}>
+            <StatefulButton
+              isDarkMode={isDarkMode}
+              isActive={pause && !disabled}
+              onClick={() => {
+                if (!disabled) setPause();
+              }}
+            >
+              Pause
+            </StatefulButton>
+          </Box>
+        </Box>
+      </Box>
     </SnakeControls>
   );
 };
 
 export default ControlsPanel;
 
-const SnakeControls = styled.div`
+const SnakeControls = styled("div")`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
-  height: 80vh;
-  article {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    height: 50vh;
-    section {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      color: white;
-    }
-  }
+  justify-content: center;
+  min-height: 50vh;
 `;
