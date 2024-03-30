b16f2984ebb4d38d9737121a129e94492198f1a3
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-31 00:49:06 +0100

    feat: add responsive behaviors to the minesweeper game board

--- src/pages/Minesweeper/components/GameBoard.tsx
+++ src/pages/Minesweeper/components/GameBoard.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import GameWrapper from "../../../shared/components/GameWrapper";
-import styled from "styled-components";
+import { styled } from "@mui/material";
+
+import FlagCircleIcon from "@mui/icons-material/FlagCircle";
+import CrisisAlertIcon from "@mui/icons-material/CrisisAlert";
+
 import {
   MinesweeperState,
   MinesweeperActionKind,
   MinesweeperAction,
 } from "../reducer";
 
-import { BsFlag } from "react-icons/bs";
-import { GiLandMine } from "react-icons/gi";
+import GameWrapper from "../../../shared/components/GameWrapper";
 
 type Props = {
   minesweeperGameState: MinesweeperState;
@@ -27,15 +28,27 @@ const GameBoard = ({
     info: string
   ) {
     if (info === "game over" && hasMine) {
-      return <GiLandMine />;
+      return (
+        <CrisisAlertIcon
+          sx={{
+            fontSize: {
+              xs: "10px",
+              sm: "20px",
+              md: "15px",
+              lg: "25px",
+              xl: "30px",
+            },
+          }}
+        />
+      );
     }
     if (!checked && hasFlag) {
-      return <BsFlag />;
+      return <FlagCircleIcon />;
     }
     if (!checked) {
       return null;
     } else if (hasMine) {
-      return <GiLandMine />;
+      return <CrisisAlertIcon />;
     } else if (numberOfMinedNeighbors === 0) {
       return null;
     } else {
@@ -44,7 +57,7 @@ const GameBoard = ({
   }
 
   return (
-    <GameWrapper>
+    <GameWrapper style={{ margin: "20px" }}>
       <MinesweeperGameBoard>
         {minesweeperGameState.board.map((row, rowIndex) => (
           <MinesweeperRow rowsNumber={minesweeperGameState.board.length}>
@@ -90,51 +103,74 @@ const GameBoard = ({
 };
 export default GameBoard;
 
-const MinesweeperGameBoard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
-  background-color: black;
-  font-size: 2rem;
-  width: 70vmin;
-  height: 70vmin;
+const MinesweeperGameBoard = styled("div")`
+  ${({ theme }) => `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+    
+    background-color: black;
+
+    font-size: 2rem;
+
+    width: 80vmin;
+    height: 80vmin;
+    @media (max-width: ${theme.breakpoints.values.md}px) {
+      font-size: 1.5rem;
+    }
+    @media (max-width: ${theme.breakpoints.values.sm}px) {
+      font-size: 1rem;
+    }
+  `}
 `;
-const MinesweeperRow = styled.div<{ rowsNumber: number }>`
+
+const MinesweeperRow = styled("div")<{ rowsNumber: number }>`
   height: ${(p) => `calc(100% / ${p.rowsNumber})`};
+
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: stretch;
   flex: 1 1 0px;
 `;
-const MinesweeperCell = styled.div<{
+
+const MinesweeperCell = styled("div")<{
   disabled: boolean;
   isEven: boolean;
   hasMine: boolean;
   isChecked: boolean;
   columnsNumber: number;
 }>`
-  /* box-sizing: border-box;
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5px); */
-
+  ${({ theme, columnsNumber, disabled, hasMine, isChecked, isEven }) => `
   color: black;
-  width: ${(p) => `calc(100% / ${p.columnsNumber})`};
+  
+  width: ${`calc(100% / ${columnsNumber})`};
+
   display: flex;
   align-items: center;
   justify-content: center;
+
+  text-align: center;
+  
   cursor: pointer;
+  
+  border: ${isChecked ? "2px dotted #7b7b7b;" : "10px groove #7b7b7b;"};
+  @media (max-width: ${theme.breakpoints.values.sm}px) {
+    border: ${isChecked ? "1.5px dotted #7b7b7b;" : "8px groove #7b7b7b;"};
+  }
+  @media (max-width: 400px) {
+    border: ${isChecked ? "1px dotted #7b7b7b;" : "5px groove #7b7b7b;"};
+  }
 
-  border: ${(p) =>
-    p.isChecked ? "2px dotted #7b7b7b;" : "10px groove #7b7b7b;"};
-
-  background: ${(p) =>
-    p.hasMine && p.isChecked
+  background: ${
+    hasMine && isChecked
       ? "red"
-      : p.isEven
+      : isEven
       ? "rgba(255, 255, 255, 0.7);"
-      : "rgba(255, 255, 255, 0.4);"};
-
-  ${(p) => (p.disabled || p.isChecked ? "" : "&:hover { background: white }")};
+      : "rgba(255, 255, 255, 0.4);"
+  };
+    
+    ${disabled || isChecked ? "" : "&:hover { background: white }"};
+  `}
 `;
