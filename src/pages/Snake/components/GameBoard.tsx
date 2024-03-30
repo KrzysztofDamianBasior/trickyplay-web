f5acdbee2163e0fdfc35ff5d09fcde10087390cf
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-03-31 00:24:39 +0100

    feat: add responsive behaviors to the snake game board, add a button to replay the game after it ends

--- src/pages/Snake/components/GameBoard.tsx
+++ src/pages/Snake/components/GameBoard.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import GameWrapper from "../../../shared/components/GameWrapper";
-import styled from "styled-components";
+import { styled } from "@mui/material/styles";
 
 import InversionApple from "../assets/apple2.png";
 import PointApple from "../assets/apple1.png";
+
+import GameWrapper from "../../../shared/components/GameWrapper";
+import ActionButton from "../../../shared/components/ActionButton";
 
 type Props = {
   wrapperRef: React.MutableRefObject<HTMLDivElement | null>;
   canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
   canvasSize: { x: number; y: number };
-  changeDirection: (e: React.KeyboardEvent<HTMLDivElement>) => void;
   disabled: boolean;
   info: string;
+  changeDirection: (e: React.KeyboardEvent<HTMLDivElement>) => void;
+  onPlayAgain: () => void;
 };
 
 const GameBoard = ({
@@ -21,16 +24,22 @@ const GameBoard = ({
   changeDirection,
   disabled,
   info,
+  onPlayAgain,
 }: Props) => {
   return (
     <GameWrapper
-      tabIndex={0}
+      tabIndex={0} // A tabindex of 0 will put the tag "in the natural tab order of the page". A higher number will give it a specific order of priority, where 1 will be the first, 2 second and so on. You can also give a tabindex of -1, which will make the div only focus-able by script, not the user.
       ref={wrapperRef}
       onKeyDown={(e) => changeDirection(e)}
     >
       {disabled && (
         <SnakeInfo>
-          <div>{info}</div>
+          <div>
+            {info}
+            {info ? (
+              <ActionButton onClick={onPlayAgain}>Play again</ActionButton>
+            ) : null}
+          </div>
         </SnakeInfo>
       )}
       <img
@@ -45,10 +54,7 @@ const GameBoard = ({
         alt="inversion apple"
         style={{ display: "none" }}
       />
-      <canvas
-        style={{
-          background: "black",
-        }}
+      <SnakeCanvas
         ref={canvasRef}
         width={`${canvasSize.x}px`}
         height={`${canvasSize.y}px`}
@@ -56,31 +62,57 @@ const GameBoard = ({
     </GameWrapper>
   );
 };
+
 export default GameBoard;
 
-const SnakeInfo = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  div {
+// const GameCanvas = styled("canvas")`
+//   background: black;
+// `;
+
+const SnakeCanvas = styled("canvas", {
+  name: "SnakeCanvas",
+  slot: "root",
+})`
+  ${({ theme }) => `
+    background-color: black;
+  `}
+`;
+
+const SnakeInfo = styled("div")`
+  ${({ theme }) => `  
     position: absolute;
-    left: 5%;
-    top: 5%;
-    width: 90%;
-    height: 90%;
-    font-size: 3rem;
+    left: 0;
+    top: 0;
 
-    /* From https://css.glass */
-    background: rgba(255, 255, 255, 0.22);
-    border-radius: 16px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(8.5px);
-    -webkit-backdrop-filter: blur(8.5px);
+    width: 100%;
+    height: 100%;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    div {
+      position: absolute;
+      left: 5%;
+      top: 5%;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+      width: 90%;
+      height: 90%;
+
+      font-size: 3rem;
+      @media (max-width: ${theme.breakpoints.values.sm}px) {
+        font-size: 2rem;
+      }
+      @media (max-width: 400px) {
+        font-size: 1.5rem;
+      }
+
+      /* From https://css.glass */
+      background: rgba(255, 255, 255, 0.22);
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(8.5px);
+      border-radius: 16px;
+    }
+  `}
 `;
