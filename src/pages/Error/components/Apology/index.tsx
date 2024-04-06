f1994cf6cf15f30088c8e5f3c190fd81e160ab05
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2024-04-06 14:55:11 +0200

    fix: fix layout problems on view with apologies

--- src/pages/Error/components/Apology/index.tsx
+++ src/pages/Error/components/Apology/index.tsx
@@ -1,5 +1,4 @@
-import { useNavigate } from "react-router-dom";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import Error from "../../../../shared/assets/other/error.png";
 import AnimatedPage from "../../../../shared/components/AnimatedPage";
@@ -7,8 +6,6 @@ import Navbar from "../../../../shared/components/Navbar";
 import Footer from "../../../../shared/components/Footer";
 
 const Apology = () => {
-  const navigate = useNavigate();
-
   return (
     <AnimatedPage>
       <Navbar />
@@ -38,30 +35,46 @@ const Apology = () => {
           sx={{
             backgroundImage: (theme) =>
               theme.palette.mode === "light"
-                ? `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" fill="%23000000"><path d="M1891 560c0-277-224-509-501-499-372 13-397 281-504 302s-215-179-511-75a459 459 0 0 0-222 218C10 809 262 886 259 992s-201 171-82 395c34 64 99 127 212 141 56 6 103 47 115 102a377 377 0 0 0 383 310c222 2 528-202 380-536-42-97 30-123 73-194s-11-120 128-156c233-61 423-246 423-494Z"></path></svg>')`
+                ? `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" fill="%23000000"><path d="M994 112c-703-2-920.47 400.35-904 905 13.35 409 32.03 946.66 977 861 684-62 792-279 835-777 61.67-714.25-288.33-987.24-908-989Z"></path></svg>')`
                 : "",
             backgroundRepeat: "no-repeat",
             backgroundSize: "80%",
             backgroundPosition: "center",
 
-            padding: { xs: "70px", sm: "120px", md: "140px", lg: "160px" },
+            padding: { xs: "80px", sm: "130px" },
           }}
         >
-          <Typography variant="h2" gutterBottom sx={{ color: "warning.main" }}>
+          <Typography
+            variant="h3"
+            gutterBottom
+            sx={{ color: "warning.main" }}
+            align="center"
+          >
             Oops!
           </Typography>
-          <Typography variant="h4" gutterBottom sx={{ color: "warning.main" }}>
-            I'm very sorry, unfortunately something is broken on the website, I
-            will try to fix it as soon as possible
-          </Typography>
-          <Button
-            variant="contained"
-            color="warning"
-            onClick={() => navigate("/")}
-            sx={{ m: 3 }}
+          <Typography
+            variant="body1"
+            gutterBottom
+            sx={{ color: "warning.main" }}
+            align="center"
           >
-            Visit the home page
-          </Button>
+            I'm very sorry,
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ color: "warning.main" }}
+            align="center"
+          >
+            unfortunately something is broken on the website :(
+          </Typography>
+          <Typography
+            variant="body1"
+            gutterBottom
+            sx={{ color: "warning.main" }}
+            align="center"
+          >
+            I will try to fix it as soon as possible
+          </Typography>
         </Box>
       </Box>
       <Footer />
