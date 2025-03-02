af5ca8e7103c3c379d36389b3561423ede6f0edf
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2025-03-02 23:37:09 +0100

    test: add UsernameUpdatedSuccessfully tests

---
+++ src/shared/components/ChangeUsernameDialog/components/__test__/UsernameUpdatedSuccessfully.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it, vi } from "vitest";
+
+import UsernameUpdatedSuccessfully from "../UsernameUpdatedSuccessfully";
+
+describe("UsernameUpdatedSuccessfully component", () => {
+  it("should render button, heading and message", async () => {
+    expect.assertions(3);
+    const onClose = vi.fn();
+
+    render(<UsernameUpdatedSuccessfully onCloseDialog={onClose} />);
+
+    // screen.getByRole("");
+    // screen.debug(undefined, undefined, { highlight: false });
+
+    const closeButton = screen.getByRole("button");
+    const title = screen.getByRole("heading");
+    const infoContent = screen.getByText(/Your username has been updated/i, {
+      exact: false,
+    }); // substring match, ignore case
+
+    expect(closeButton).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+    expect(infoContent).toBeInTheDocument();
+  });
+
+  it("should properly trigger the onClose event handler", async () => {
+    expect.assertions(1);
+    const onClose = vi.fn();
+
+    render(<UsernameUpdatedSuccessfully onCloseDialog={onClose} />);
+
+    // screen.getByRole("");
+    // screen.debug(undefined, undefined, { highlight: false });
+
+    const closeButton = screen.getByRole("button");
+
+    await userEvent.click(closeButton);
+
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
+});
